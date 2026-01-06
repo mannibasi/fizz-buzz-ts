b979e0e9b0d952031fc0172fca503c7331af466b
Manni Basi <manni.basi@gmail.com>
2026-01-06 22:07:12 +0000

    Re-factored into a more functional style with 'explanatory methods'

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,6 +1,15 @@
+const isDivisibleByThree = (integer: number) => integer % 3 === 0;
+const isDivisibleByFive = (integer: number) => integer % 5 === 0;
+
 export const fizzBuzz = (integer: number) => {
-    if (integer % 3 === 0 && integer % 5 === 0) return 'FizzBuzz';
-    if (integer % 3 === 0) return 'Fizz';
-    if (integer % 5 === 0) return 'Buzz';
+    if (isDivisibleByThree(integer) && isDivisibleByFive(integer)) {
+        return 'FizzBuzz';
+    }
+    if (isDivisibleByThree(integer)) {
+        return 'Fizz';
+    }
+    if (isDivisibleByFive(integer)) {
+        return 'Buzz';
+    }
     return integer.toString();
 };
