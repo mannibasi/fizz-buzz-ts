b09cf9e5ac9cab7ea4c589058df94307afbf6a69
Manni Basi <manni.basi@gmail.com>
2026-01-06 11:34:28 +0000

    Added failing test for the number 2

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -3,5 +3,8 @@ import { fizzBuzz } from '../src/fizzBuzz';
 describe('Fizz Buzz', () => {
   it('convert the number 1 to the string "1"', () => {
     expect(fizzBuzz(1)).toBe("1");
-  })
-})
+  });
+  it('convert the number 2 to the string "2"', () => {
+    expect(fizzBuzz(2)).toBe("2");
+  });
+});
