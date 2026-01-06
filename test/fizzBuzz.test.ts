9e8c25462002017944f067959d3cc39a4ff0233c
Manni Basi <manni.basi@gmail.com>
2026-01-06 21:43:13 +0000

    Refactor of tests for clarity/readability

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,32 +1,32 @@
 import { fizzBuzz } from '../src/fizzBuzz';
 
-describe.each([
-    {input: 1, expected: "1"},
-    {input: 2, expected: "2"},
-    {input: 4, expected: "4"},
-])('Fizz Buzz', ({input, expected}) => {
-  test(`convert the number ${input} to the string "${expected}"`, () => {
-    expect(fizzBuzz(input)).toBe(expected);
-  });
-});
-
 describe.each([3, 6, 9])
-('Fizz Buzz', (input) => {
-  test(`${input} returns Fizz`, () => {
+('Fizz Buzz converts multiples of three to Fizz', (input) => {
+  test(`converts ${input} to Fizz`, () => {
     expect(fizzBuzz(input)).toBe("Fizz");
   });
 });
 
 describe.each([5, 10, 20])
-('Fizz Buzz', (input) => {
-    test(`${input} returns Buzz`, () => {
+('Fizz Buzz converts multiples of five to Buzz', (input) => {
+    test(`converts ${input} to Buzz`, () => {
         expect(fizzBuzz(input)).toBe("Buzz");
     });
 });
 
 describe.each([15, 30, 45])
-('Fizz Buzz', (input) => {
-    test(`${input} returns FizzBuzz`, () => {
+('Fizz Buzz converts multiples of three and five to FizzBuzz', (input) => {
+    test(`converts ${input} to FizzBuzz`, () => {
         expect(fizzBuzz(input)).toBe("FizzBuzz");
+    });
+});
+
+describe.each([
+    {input: 1, expected: "1"},
+    {input: 2, expected: "2"},
+    {input: 4, expected: "4"},
+])('Fizz Buzz converts all other numbers to a string representation of themselves', ({input, expected}) => {
+    test(`converts ${input} to "${expected}"`, () => {
+        expect(fizzBuzz(input)).toBe(expected);
     });
 });
