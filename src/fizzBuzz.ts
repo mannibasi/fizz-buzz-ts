adf793f9b172075557350f5d1200dd744759dab7
Manni Basi <manni.basi@gmail.com>
2026-01-06 21:09:18 +0000

    Simple change to get back to green

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,4 +1,4 @@
 export const fizzBuzz = (integer: number) => {
-    if (integer === 3 || integer === 6) return 'Fizz';
+    if (integer === 3 || integer === 6 || integer === 9) return 'Fizz';
     return integer.toString();
 };
