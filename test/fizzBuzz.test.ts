2f70343942b9f0616559d39bb8dc8c51fd9611ad
Manni Basi <manni.basi@gmail.com>
2026-01-06 11:40:48 +0000

    Added failing test for the number 4

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -7,4 +7,7 @@ describe('Fizz Buzz', () => {
   it('convert the number 2 to the string "2"', () => {
     expect(fizzBuzz(2)).toBe("2");
   });
+  it('convert the number 4 to the string "4"', () => {
+    expect(fizzBuzz(4)).toBe("4");
+  });
 });
